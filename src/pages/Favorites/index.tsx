c2f5d8c475e2cc4731a23279e828132752c7e28a
Alex <alex.sousa20@hotmail.com>
2020-08-09 11:16:02 -0300

    mudança de png

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -4,14 +4,14 @@ import { View } from 'react-native';
 import styles from './styles'
 import PageHeader from '../../components/PageHeader';
 
-function Favorites(){
-    return 
-    (
-        <View style={styles.container}>
-          <PageHeader title="Meus Proffys favoritos"/>      
 
-        </View>
-        )
+function Favorites(){
+  return (
+      <View style={styles.container}>
+        <PageHeader title="Meus Proffys favoritos "/>      
+
+      </View>
+      )
 }
 
 export default Favorites;
